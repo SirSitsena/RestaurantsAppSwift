fb63b7198712583b4d6dea9558983b002c4bb177
Anestis Cheimonettos <anestisukr@gmail.com>
2021-12-07 15:03:01 +0100

    Initial Commit

---
+++ Lab02Restaurant/Lab02RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab02RestaurantApp.swift
+//  Lab02Restaurant
+//
+//  Created by Anestis Cheimonettos on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct Lab02RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
